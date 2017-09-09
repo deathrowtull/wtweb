e77fae0fb6fc38adf2a4398f3b82354cde73ad72
deathrowtull <eamartinez1@buffs.wtamu.edu>
2017-09-09 02:31:08 -0500

    initial

---
+++ app/js/init.js
@@ -0,0 +1,36 @@
+jQuery(window).scroll(function(e) {
+
+	//var scroll = getScrollOffsets();
+    if (window.scrollTop >= 50) {
+        $('#logo-image').attr('src', 'img/Acme_Monogram_Colour.png')
+        $(".scroll-hide").addClass("bananana");
+    }
+    if (window.scrollTop < 50) {
+        $(".scroll-hide").removeClass("hide");
+        $('#logo-image').attr('src', 'img/Acme_Colour.png');
+        //console.log("hide");
+    }
+    console.log(window.scrollTop());
+
+	   function getScrollOffsets() {
+		// This works for all browsers except IE versions 8 and before
+		if ( window.pageXOffset != null ) {
+			return window.pageYOffset;
+
+		}
+
+		// For browsers in Standards mode
+		var doc = window.document;
+		if ( document.compatMode === "CSS1Compat" ) {
+
+			return doc.documentElement.scrollTop;
+		}
+
+
+		// For browsers in Quirks mode
+
+		return doc.body.scrollTop;
+	}
+});
+
+
